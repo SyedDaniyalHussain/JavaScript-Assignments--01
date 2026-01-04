df23627817c63029ba7415d2372cb168edaeefc6
SyedDaniyalHussain <sdaniyalh4@gmail.com>
2026-01-05 02:00:43 +0500

    files pasted

---
+++ app.js
@@ -0,0 +1,103 @@
+// Chapter 01 >  Question No: 01  //
+// alert("Welcome to our Website");
+
+// Chapter 01 >  Question No: 02  //
+// alert("Error! Please enter a valid Password");
+
+// Chapter 01 >  Question No: 03  //
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// Chapter 01 >  Question No: 04  //
+// alert("Welcome top JS Land");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs");
+
+// Chapter 02 >  Question No: 01  //
+// var username;
+
+// Chapter 02 >  Question No: 02  //
+// var myName="Syed Daniyal Hussain";
+
+// Chapter 02 >  Question No: 03  //
+// var TitledMessage = "Hello World!";
+// alert(TitledMessage);
+
+// Chapter 02 >  Question No: 04  //
+// var Name = "Daniyal"
+// var age = 22;
+// var goal = "Web Developer soon InshaALLAH";
+// alert(Name);
+// alert(age);
+// alert(goal);
+
+// Chapter 02 >  Question No: 05  //
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// // Chapter 02 >  Question No: 06  //
+// var email = "sdaniaylh00@gmail.com"
+// alert("My email address is : " + email);
+
+// // Chapter 02 >  Question No: 07  //
+// var myVar = "A smarter \nway to learn JavaScript";
+// alert("I am trying to learn from book" + " " + myVar);
+
+// Chapter 02 >  Question No: 08  //
+// var myVar = "Yeah! I can write HTML content through JavaScript";
+// document.write(myVar);
+
+// Chapter 02 >  Question No: 09  //
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// Chapter 03 >  Question No: 01  //
+// var myAge = "My age is 22";
+// alert(myAge);
+
+// Chapter 03 >  Question No: 02  //
+// var a = prompt("you have visited this website times?");
+// alert("you have visited this website" + " " + a + " " + "times");
+
+// Chapter 03 >  Question No: 03  //
+// var B = prompt("My birth Year is:");
+// document.write("My birth Year is: " + B);
+
+// var a = "My birth year is 2003";
+// document.write(a); 
+
+// Chapter 03 >  Question No: 04  //
+// var VisitorsName = prompt("enter visitors Name");  
+// var ProductTitle = prompt("Enter product title");
+// var Quantity =  prompt("enter prducty quantity");
+// console.log(VisitorsName, ProductTitle, Quantity);
+// document.write(VisitorsName + " " + "ordered" + " " + Quantity,  " " + ProductTitle + " " + "from Delta Tech");
+
+
+// chapter 04 > Question No: 01 
+// var MyName;
+// var BigBroName;
+// var SchoolName;
+
+//chapter 04 > Question No: 02
+   //Legal variable Names:-
+// var MyNAme;
+// var My_Name;
+// var $name;
+// var Money_in_$;
+// var MoneyIn$;
+//var Name1; 
+ 
+  //Illegal variable names:-
+// var 1name;
+// var -MyName;
+// var My&Name;
+// var =MyAge;
+// var name+;
+
+
+//chapter 04 > Question No: 03 
+// document.write("<h1>Rules for naming JS variables</h1>" + 
+//                 "Variable names can only contain numbers, $, and _. For example: $my_1stVariable <br>" + 
+//                 "Variables must begin with a letter, $ or _. For example $name, _name or name <br>" + 
+//                 "Variable names are case sensitive <br>" + 
+//                 "Variable names should not be JS keywords.");
+
+
+
